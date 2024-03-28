10a50685f4d4ffb5ae25e782d45850027754ed52
Tteuw2k24 <165336247+Tteuw2k24@users.noreply.github.com>
2024-03-28 19:47:04 -0300

    Update Conversor de Notas.py

--- Conversor de Notas.py
+++ Conversor de Notas.py
@@ -1,14 +1,14 @@
 nota = float(input('Digite sua nota:'))
-if (nota > 90) or (nota < 100):
-        print ('Sua Nota é A')
+if nota >= 90 or nota <= 100:
+        print ('Sua Nota é A');
 else:
- if (nota > 80) or (nota < 89):
-        print('Sua Nota é B')
+ if nota > 80 <= 89:
+        print('Sua Nota é B');
  else:
-        if (nota > 70) or (nota < 79):
-            print('Sua Nota é C')
+        if nota >= 70 <= 79:
+            print('Sua Nota é C');
 else:
-if (nota > 60) or (nota < 69):
-        print('Sua Nota é D')
+if nota >= 60 <= 69:
+        print('Sua Nota é D');
  else:
     print('Sua Nota é f')
