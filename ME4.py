8c04bbeb20f5842150047fe3a264b3f68e06c2c4
Tteuw2k24 <165336247+Tteuw2k24@users.noreply.github.com>
2024-03-28 20:07:53 -0300

    Update ME4.py

--- ME4.py
+++ ME4.py
@@ -1,6 +1,10 @@
-n = float(input('Digite um Numero:0'))
-n1 = float(input('Digite um Numero:0'))
-n2 = float(input('Digite um Numero:0'))
-if n > n1:
-    print (maior)
-else
+primeiro = float(input('Digite um Numero:'))
+segundo = float(input('Digite um Numero:'))
+terceiro = float(input('Digite um Numero:'))
+maior = primeiro
+
+if segundo > maior:
+    maior = segundo
+if terceiro > maior:
+    maior = terceiro
+print (maior)
